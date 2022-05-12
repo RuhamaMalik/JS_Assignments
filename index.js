607ffff4980df7d03c707de940be8b5a58dfe4ce
RuhamaMalik <malikruhama7@gmil.com>
2022-05-12 10:37:42 -0800

    chapter#6

---
+++ index.js
@@ -0,0 +1,55 @@
+//Assignment # 6(MATH EXPRESSIONS)
+
+//Q1
+  /*var a = 10;
+  document.write("<h3>Result:</h3> The Value of a is : " + a + "</br><b>..................................................</b> </br></br>");
+  
+  document.write(" The value of ++a is : " + ++a);
+  document.write("</br> Now the value of a  is : " + a);
+  document.write("</br></br> The value of a++  is : " + a++);
+  document.write("</br> Now the value of a  is : " + a);
+  document.write("</br></br> The value of --a  is : " + --a);
+  document.write("</br> Now the value of a  is : " + a);
+  document.write("</br></br> The value of a--  is : " + a--);
+  document.write("</br> Now the value of a  is : " + a);*/
+
+//Q2
+  /*var a = 2;
+  var b = 1;
+  var result = --a - --b + ++b + b--;
+  
+  document.write("a is " + a + "</br> b is " + b + "</br> result is " + result);*/
+
+//Q3
+  /*var name = prompt("Enter Your name ", " Ruhama");
+  document.write("Hello " + name + " !");*/
+
+//Q5
+  /*var num = +prompt("Enter number" , 5);
+  document.write("table of " + num + "</br></br>");
+  for(i = 1; i<=10; i++){
+      document.write(num + " * " + i + " = " + (num*i) + "</br>");
+  }*/
+
+//Q6 
+  /*var subject1 = prompt(" Enter Subject 1");
+  var subject2 = prompt(" Enter Subject 2");
+  var subject3 = prompt(" Enter Subject 3");
+  var eachSubMarks = 100;
+  var sub1_marks = +prompt("Enter Your Obtained marks of Subject 1");
+  var sub2_marks = +prompt("Enter Your Obtained marks of Subject 2");
+  var sub3_marks = +prompt("Enter Your Obtained marks of Subject 3");
+  var obtained_marks = sub1_marks + sub2_marks + sub3_marks;
+  var total_marks = eachSubMarks + eachSubMarks + eachSubMarks;
+  var percentage = (obtained_marks / total_marks) * 100;
+  var sub1_percentage = (sub1_marks /eachSubMarks)*100;
+  var sub2_percentage = (sub2_marks /eachSubMarks)*100;
+  var sub3_percentage = (sub3_marks /eachSubMarks)*100;
+  
+  document.write("<table border=2px;>" +
+     "<tr>"+"<th>"+ "Subject" + "</th>" + "<th>" + "Total Marks" + "</th>" + "<th>" + "Obtained Marks" + "</th>" + "<th>" + "Percentage" + "</th>" + "</tr>" +  
+  "<tr>" + "<td>" + subject1 + "</td>"+ "<td>" + eachSubMarks + "</td>"+ "<td>" + sub1_marks + "</td>" + "<td>" + sub1_percentage + "%" + "</td>" +
+   "</tr>" +  "<tr>" + "<td>" + subject2 + "</td>"+ "<td>" + eachSubMarks + "</td>"+ "<td>" + sub2_marks + "</td>" + "<td>" + sub2_percentage + "%" + "</td>" + "</tr>" + 
+    "<tr>" + "<td>" + subject3 + "</td>"+ "<td>" + eachSubMarks + "</td>"+ "<td>" + sub3_marks + "</td>" + "<td>" + sub3_percentage + "%" + "</td>" + "</tr>"+ 
+    "<tr>" +"<td>" + " " + "</td>" + "<td>" +"<center>" + "<b>" + total_marks +"</center>" + "</b>"+"</td>"+ "<td>" +"<center>" + "<b>"+ obtained_marks+"</center>" + "</b>"+  "</td>" + "<td>" +"<center>" + "<b>"+ percentage + "%" +"</center>" + "</b>"+ "</td>" + "</tr>"+
+    "<table>");*/ 
