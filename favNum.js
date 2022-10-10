afaa8cb287d3cf3338ea1d64de934075120cbc62
RuhamaMalik <83574511+RuhamaMalik@users.noreply.github.com>
2022-10-10 01:42:12 -0700

    ----

--- favNum.js
+++
@@ -1,8 +0,0 @@
-//////////// 9 - Favorite Number
-// Store your favorite number in a variable. Then, 
-// using that variable, create a message that reveals your favorite number.
-//  Print that message.
-
-let favNum = 4;
-console.log(`My favourite number is ${favNum}`)
-
